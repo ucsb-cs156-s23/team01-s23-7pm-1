46001b5eae5d1c76354ebddd722cd647c5b67875
Christian Baker <christianjbaker@icloud.com>
2023-04-25 00:33:21 -0700

    cb - add fixtures for park objects

---
+++ frontend/src/fixtures/parkFixtures.js
@@ -0,0 +1,42 @@
+const parkFixtures = {
+    onePark:
+    [
+        {
+            "id": 1,
+            "name": "Yosemite National Park",
+            "state": "California",
+            "acres": 761747,
+            "description": "Yosemite National Park is a national park located in the western Sierra Nevada region of California, known for its scenic natural beauty featuring granite cliffs, waterfalls, giant sequoias, and diverse wildlife."
+        }
+    ],
+
+    threeParks:
+    [
+        {
+            "id": 2,
+            "name": "Grand Canyon National Park",
+            "state": "Arizona",
+            "acres": 1218375,
+            "description": "Grand Canyon National Park is a national park located in northern Arizona, known for its spectacular landscape of deep canyons and colorful rock formations, carved over millions of years by the Colorado River."
+        },
+
+        {
+            "id": 3,
+            "name": "Zion National Park",
+            "state": "Utah",
+            "acres": 147242,
+            "description": "Zion National Park is a national park located in southwestern Utah, known for its magnificent sandstone cliffs, narrow canyons, and diverse plant and animal life."
+        },
+
+        {
+            "id": 4,
+            "name": "Grand Teton National Park",
+            "state": "Wyoming",
+            "acres": 310044,
+            "description": "Grand Teton National Park is a national park located in northwestern Wyoming, known for its towering mountain peaks, glacial lakes, and abundant wildlife."
+        },
+
+    ]
+};
+
+export { parkFixtures };
